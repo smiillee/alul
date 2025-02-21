4dc07a5f042bcf3f8e2539d89cdc9d7af55e8721
smiillee <75018014+smiillee@users.noreply.github.com>
2025-02-21 23:21:52 +0700

    Add files via upload

---
+++ belajar.js
@@ -0,0 +1,8 @@
+function tombol() {
+    var klik = document.getElementById("gambar");
+    if (klik.style.display === "block") {
+        klik.style.display = "none";
+    } else {
+        klik.style.display = "block";
+    }
+}
